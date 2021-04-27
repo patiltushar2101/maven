b101fd3bad550ef5a119e56de9af888700dae6e9
Patil Tushar <patiltushar2101@gmail.com>
2021-04-28 01:03:21 +0530

    all

--- src/frontend/pages/UpdateProfile.js
+++ src/frontend/pages/UpdateProfile.js
@@ -3,6 +3,7 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 import UpdateProfile from "../components/UpdateProfile";
 import "./styles.css"
+import 'firebase/storage';
 
 export default function f() {
     return (
